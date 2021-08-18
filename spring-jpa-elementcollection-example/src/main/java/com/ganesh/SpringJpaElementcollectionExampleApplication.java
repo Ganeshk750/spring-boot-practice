d3b3b9ebdb3277c97445f70654853c7fd21e34e4
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-18 15:47:48 +0530

    ##

--- spring-jpa-elementcollection-example/src/main/java/com/ganesh/SpringJpaElementcollectionExampleApplication.java
+++ spring-jpa-elementcollection-example/src/main/java/com/ganesh/SpringJpaElementcollectionExampleApplication.java
@@ -1,13 +1,47 @@
 package com.ganesh;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ganesh.model.Address;
+import com.ganesh.model.User;
+import com.ganesh.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
-public class SpringJpaElementcollectionExampleApplication {
+@RequiredArgsConstructor
+public class SpringJpaElementcollectionExampleApplication implements CommandLineRunner {
+	
+    private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaElementcollectionExampleApplication.class, args);
 	}
 
+	 @Override
+	    public void run(String... args) throws Exception {
+	        // Cleanup database tables.
+	        userRepository.deleteAll();
+
+	        // Insert a user with multiple phone numbers and addresses.
+	        Set<String> phoneNumbers = new HashSet<>();
+	        phoneNumbers.add("+91-9999999999");
+	        phoneNumbers.add("+91-9898989898");
+
+	        Set<Address> addresses = new HashSet<>();
+	        addresses.add(new Address("747", "Golf View Road", "Bangalore",
+	                "Karnataka", "India", "560008"));
+	        addresses.add(new Address("Plot No 44", "Electronic City", "Bangalore",
+	                "Karnataka", "India", "560001"));
+
+	        User user = new User("Rajeev Kumar Singh", "rajeev@callicoder.com",
+	                phoneNumbers, addresses);
+
+	        userRepository.save(user);
+	    }
 }
--- spring-jpa-elementcollection-example/src/main/java/com/ganesh/model/User.java
+++ spring-jpa-elementcollection-example/src/main/java/com/ganesh/model/User.java
@@ -19,7 +19,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -27,7 +26,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -55,6 +53,16 @@ public class User {
             @AttributeOverride(name = "addressLine2", column = @Column(name = "street"))
     })
     private Set<Address> addresses = new HashSet<>();
+
+    public User(String name, String email, Set<String> phoneNumbers, Set<Address> addresses) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumbers = phoneNumbers;
+        this.addresses = addresses;
+    }
+
+    
+    
     
 }
 
